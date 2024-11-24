b59929396953a203bc91aaad18ec8423d22f7443
undefined <undefined_1@outlook.com>
2024-11-24 23:56:39 +0800

    fix(crc32): fix CRC32 implementation

--- src/utils/tests/crc32.rs
+++ src/utils/tests/crc32.rs
@@ -10,5 +10,23 @@ fn test_can_calcualte_crc32() {
             panic!()
         },
     };
-    assert_eq!(crc32, 0x7A2A208B, "CRC32 is not correct {} != {}", crc32, 0x7A2A208B);
+    assert_eq!(crc32, 0x7A2A208B, "CRC32 is not correct {:#X} != {:#X}", crc32, 0x7A2A208B);
+}
+
+#[allow(dead_code)]
+fn generate_crc32_table() {
+    let poly: u32 = 0xedb88320;
+    let mut table: [u32; 256] = [0; 256];
+    for i in 0..256 {
+        let mut crc = i as u32;
+        for _ in 0..8 {
+            crc = if crc & 1 != 0 {
+                (crc >> 1) ^ poly
+            } else {
+                crc >> 1
+            }
+        }
+        table[i] = crc.to_le() as u32;
+    };
+    println!("{:#X?}", table);
 }
